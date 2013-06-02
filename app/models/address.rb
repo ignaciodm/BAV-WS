379dfa18b143502b4f1face06dc2f18ce39cb9be
Ignacio De Maio <ignacio@zauberlabs.com>
2013-06-02 19:44:54 -0300

    Se agrega floor y flat a Address

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,9 +1,10 @@
 class Address < ActiveRecord::Base
-  attr_accessible :details, :name, :number, :street
+  attr_accessible :details, :name, :number, :street, :floor, :flat
 
   validates :name, :presence => true, :length => { :minimum => 2 }
   validates :street, :presence => true, :length => { :minimum => 2 }
   validates :number, :numericality => true
-
+  validates :floor, numericality: {only_integer: true}, :allow_nil => true
+  validates :flat, :length => {:maximum => 25}
 
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -2,15 +2,12 @@ require 'spec_helper'
 
 describe Address do
 
-  before(:each) do
-  end
-
   describe 'Validations' do
+    address = Address.create({name: nil, street: 'street', number: 10})
 
     describe 'name' do
       context 'when name is nil' do
         it 'should not save' do
-          address = Address.create({name: nil, street: 'street', number: 10})
           address.errors[:name].should == ["can't be blank", "is too short (minimum is 2 characters)"]
         end
       end
@@ -74,6 +71,81 @@ describe Address do
           address.errors.empty?.should == true
         end
       end
+    end
+
+    describe 'floor' do
+      context 'when floor is nil' do
+        it 'should save' do
+          address = Address.create({name: 'name', street: 'street', number: 10, floor: nil})
+          address.errors.empty?.should == true
+        end
+      end
+
+      context 'when floor is empty' do
+        it 'should not save' do
+          address = Address.create({name: 'name', street: 'street', number: 10, floor: ''})
+          address.errors.empty?.should == true
+        end
+      end
+
+      context 'when floor is a number' do
+        it 'should save' do
+          address = Address.create({name: 'name', street: 'street', number: 10, floor: 1})
+          address.errors.empty?.should == true
+        end
+      end
+
+      context 'when floor is not a number' do
+        it 'should save' do
+          address = Address.create({name: 'name', street: 'street', number: 10, floor: 'floor'})
+          address.errors[:floor][0].should == "is not a number"
+        end
+      end
+
+      context 'when floor is not an integer' do
+        it 'should save' do
+          address = Address.create({name: 'name', street: 'street', number: 10, floor: 1.3})
+          address.errors[:floor][0].should == "must be an integer"
+        end
+      end
+    end
+
+    describe 'flat' do
+      context 'when flat is nil' do
+        it 'should save' do
+          address = Address.create({name: 'name', street: 'street', number: 10, flat: nil})
+          address.errors.empty?.should == true
+        end
+      end
+
+      context 'when flat is empty' do
+        it 'should not save' do
+          address = Address.create({name: 'name', street: 'street', number: 10, flat: ''})
+          address.errors.empty?.should == true
+        end
+      end
+
+      context 'when flat is a number' do
+        it 'should convert it to integer' do
+          address = Address.create({name: 'name', street: 'street', number: 10, flat: 1})
+          address.errors.empty?.should == true
+        end
+      end
+
+      context 'when flat is not valid string' do
+        it 'should save' do
+          address = Address.create({name: 'name', street: 'street', number: 10, flat: 'H'})
+          address.errors.empty?.should == true
+        end
+      end
+
+      context 'when flat is too long' do
+        it 'should save' do
+          address = Address.create({name: 'name', street: 'street', number: 10, flat: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'})
+          address.errors[:flat].should == ["is too long (maximum is 25 characters)"]
+        end
+      end
+
     end
 
   end
