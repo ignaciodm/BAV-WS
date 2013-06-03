08184c18e6fe90bc87cb5c6a42df2180b9ab0c0f
Ignacio De Maio <ignacio@zauberlabs.com>
2013-06-03 00:55:56 -0300

    Se agrega un setup basico de provincias, partidos y localidades

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,54 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+    provincias = [{nombre: 'Capital Federal',
+                   partidos: [{nombre: 'San Isidro',
+                               localidades: ['Villa Adelina', 'Boulogne', 'Martinez', 'Acassuso', 'San Isidro', 'Beccar']}]},
+                  {nombre: 'Buenos Aires'},
+                  {nombre: 'Catamarca'},
+                  {nombre: 'Chaco'},
+                  {nombre: 'Chubut'},
+                  {nombre: 'Cordoba'},
+                  {nombre: 'Corrientes'},
+                  {nombre: 'Entre Rios'},
+                  {nombre: 'Formosa'}, 
+                  {nombre: 'Jujuy'},
+                  {nombre: 'La Pampa'}, 
+                  {nombre: 'La Rioja'}, 
+                  {nombre: 'Mendoza'},
+                  {nombre: 'Misiones'},
+                  {nombre: 'Neuquen'},
+                  {nombre: 'Rio Negro'},
+                  {nombre: 'Salta'},
+                  {nombre: 'San Juan'},
+                  {nombre: 'San Luis'},
+                  {nombre: 'Santa Cruz'},
+                  {nombre: 'Santa Fe'},
+                  {nombre: 'Santiago del Estero'},
+                  {nombre: 'Tierra del Fuego'},
+                  {nombre: 'Tucuman'}]
+
+    provincias.each do |provincia|
+      partidos = provincia[:partidos]
+      provincia = Provincia.create!({nombre: provincia[:nombre]})
+
+      if partidos
+
+        partidos.each do |partido|
+          localidades = partido[:localidades]
+          partido = Partido.create!({nombre: partido[:nombre], provincia_id: provincia.id})
+
+          if localidades
+            localidades.each do |localidad|
+              Localidad.create!({nombre: localidad, partido_id: partido.id })
+            end
+          end
+
+        end
+
+      end
+    end
+
+
+
