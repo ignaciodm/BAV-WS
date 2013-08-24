63d0c42bbadf27d4e4b73ab69cc781afc043251f
Ignacio De Maio <ignacio@zauberlabs.com>
2013-08-24 12:25:40 -0300

    Se elimina la inicializacion y coneccion a la base de datos en la precompilacion de assets

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,12 @@ module BAVWs
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    #While precompiling assets, in Rails 3.x, you can prevent
+    # initializing your application and connecting to the database
+    # by ensuring that the following line is in your config/application.rb:
+    # Based on https://devcenter.heroku.com/articles/rails-asset-pipeline#troubleshooting
+    config.assets.initialize_on_precompile = false  #Heroku falla sin esta linea
+
   end
 end
