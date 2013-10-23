ed62de94c4c6f70a60e1d95e6471e3f690ddc030
Ignacio De Maio <ignacio@zauberlabs.com>
2013-10-22 23:09:10 -0300

    active admin para denuncia

--- app/models/denuncia.rb
+++ app/models/denuncia.rb
@@ -2,7 +2,8 @@ class Denuncia < ActiveRecord::Base
   attr_accessible :comisaria_id,
                   :direccion_id,
                   :estado,
-                  :observacion
+                  :observacion,
+                  :as => [:default, :admin]
 
   belongs_to :direccion
   belongs_to :comisaria
