da31824b3c32a7ff502c975f62c0effb6f196837
Ignacio De Maio <ignacio@zauberlabs.com>
2013-10-26 15:55:36 -0300

    textos y error en cambiar password

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -6,13 +6,20 @@ class PasswordsController < DeviseController
   # POST /resource/password
   def create
     #Formateo de parametros necesarios
-    params[:usuario] = params[:password]  #No se porque no setea el scope del usuario, setea el param password asi que no asigno a mano
-    self.resource = resource_class.send_reset_password_instructions(resource_params)
 
-    if successfully_sent?(resource)
-      render :json => {}, status: :ok
+    usuario = Usuario.find_by_email(params[:email])
+
+    if usuario
+      params[:usuario] = params[:password]  #No se porque no setea el scope del usuario, setea el param password asi que no asigno a mano
+      self.resource = resource_class.send_reset_password_instructions(resource_params)
+
+      if successfully_sent?(resource)
+        render :json => {}, status: :ok
+      else
+        render :json => {error: 'No se pudo enviar el mail para cambiar el password'}, status: :internal_server_error
+      end
     else
-      render :json => {error: 'No se pudo enviar el mail para cambiar el password'}, status: :internal_server_error
+      render :json => {email: 'El email ingresado no se encuentra registrado en el sistema'}, status: :internal_server_error
     end
   end
 
